7970cc6787fbb5e8001cda2a2a12eb5dba642994
Patrick Savio <PatrickS@treedigit.com>
2024-02-02 20:35:47 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    trailingSlash: true,
-    distDir: 'build',
+    trailingSlash: true
     optimizeFonts: false,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
