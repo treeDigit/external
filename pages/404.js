ab003db468db247c56ecf6011edaf641442926b5
treeDigit <158580354+treeDigit@users.noreply.github.com>
2024-02-04 16:12:38 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -13,7 +13,7 @@ class Error extends Component {
                                 <div className="error-content">
                                     <img src="/images/404.png" alt="error" />
                 
-                                    <h3>Page Not Found</h3>
+                                    <h3>{process.env.MY_VAR}</h3>
                                     <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
 
                                     <Link href="/">
